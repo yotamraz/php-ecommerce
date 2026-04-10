4b8b309be05d332ecf5d8ef5ec5094daf7cc2769
mcode-bot@modelcode.ai <mcode-bot@modelcode.ai>
2026-04-10 20:47:34 +0000

    Origin spec validation (milestone 3)

--- .mcode/tests/functional_testing/m3-b807d3d1-test_protocol_spec.py
+++ .mcode/tests/functional_testing/m3-b807d3d1-test_protocol_spec.py
@@ -9,7 +9,7 @@ This script supports two modes:
 1. SRC Validation: Tests endpoints and captures responses (no expected_response)
 2. DST Contract Validation: Tests endpoints and validates responses match expected (has expected_response)
 
-Generated at: 2026-04-10T20:45:40.635479+00:00
+Generated at: 2026-04-10T20:47:31.393295+00:00
 Project: php-ecommerce
 Milestone: 3
 """
